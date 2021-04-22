1bf827e6fd9f5c1a48f4d570327be542f1c67f7e
asha joseph <ashajo183@gmail.com>
2021-04-22 15:19:01 +0200

    BookRepository created and  functionalities added

---
+++ src/main/java/se/lexicon/booklender/data/BookRepository.java
@@ -0,0 +1,14 @@
+package se.lexicon.booklender.data;
+
+import org.springframework.data.repository.CrudRepository;
+import se.lexicon.booklender.Book;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book,Integer> {
+
+    List<Book>findByReservedIgnoreCase (boolean reserved);
+    List<Book>findByAvailableIgnoreCase(boolean available);
+    List<Book>findByTitleIgnoreCase(String title);
+
+}

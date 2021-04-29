6b05fef32bd6bc880ce085b6ac63ab08fef4b9b6
asha joseph <ashajo183@gmail.com>
2021-04-29 15:17:08 +0200

    LoanDto class created

---
+++ src/main/java/se/lexicon/booklender/dto/LoanDto.java
@@ -0,0 +1,14 @@
+package se.lexicon.booklender.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class LoanDto {
+    private long loanId;
+    private LibraryUserDto loanTaker;
+    private BookDto book;
+    private LocalDate loanDate;
+    private boolean terminated;
+}

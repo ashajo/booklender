4b88cac9f27ae2645aee041a20d9045069f13741
asha joseph <ashajo183@gmail.com>
2021-04-29 15:03:12 +0200

    LibraryUserDto class created

---
+++ src/main/java/se/lexicon/booklender/dto/LibraryUserDto.java
@@ -0,0 +1,15 @@
+package se.lexicon.booklender.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+
+@Data
+public class LibraryUserDto {
+    private int userId;
+    private LocalDate regDate;
+    private String name;
+    private String email;
+
+}
